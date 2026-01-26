e6a3686f664d02c7de26a63295c5d66c579ac038
Samiran2004 <samiran.samanta.dev@gmail.com>
2026-01-27 00:14:37 +0530

    add/Basic Setup<Samiran>

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './config/swagger.js';
+import errorHandler from './middlewares/errorHandler.js';
+import routes from './routes/index.js';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Swagger Docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes
+app.use('/api', routes);
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Error Handler
+app.use(errorHandler);
+
+export default app;
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/health', (req, res) => {
+    res.status(200).json({ status: 'UP' });
+});
+
+export default router;
